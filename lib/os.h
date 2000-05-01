cb367abf983c0523ac8f229795d54c808eb355ab
jon <jon@xiph.org>
2000-05-01 05:46:47 +0000

    Added an ifdef for compilation in win32.
    svn path=/trunk/vorbis/; revision=351

--- lib/os.h
+++ lib/os.h
@@ -1,3 +1,5 @@
+#ifndef _OS_H
+#define _OS_H
 /********************************************************************
  *                                                                  *
  * THIS FILE IS PART OF THE Ogg Vorbis SOFTWARE CODEC SOURCE CODE.  *
@@ -12,7 +14,7 @@
  ********************************************************************
 
  function: #ifdef jail to whip a few platforms into the UNIX ideal.
- last mod: $Id: os.h,v 1.2 1999/12/30 07:26:46 xiphmont Exp $
+ last mod: $Id: os.h,v 1.3 2000/05/01 05:46:47 jon Exp $
 
  ********************************************************************/
 
@@ -30,10 +32,19 @@
 
 #ifndef alloca
 #ifdef _WIN32
-#define alloca(x) (_alloca(x))
+#  define alloca(x) (_alloca(x))
 #endif
+#endif
+
+#ifdef _WIN32
+typedef __int64 int64_t;
+typedef unsigned int u_int32_t;
+#  define FAST_HYPOT(a, b) sqrt((a)*(a) + (b)*(b))
+#else // if not _WIN32
+#  define FAST_HYPOT hypot
 #endif
 
 #endif
 
+#endif // _OS_H
 
