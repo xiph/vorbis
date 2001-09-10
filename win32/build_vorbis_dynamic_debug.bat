f0f669dd399fc1b71173b26b9b6c30d7f81403a0
cwolf <cwolf@xiph.org>
2001-09-10 07:14:24 +0000

    Change include paths relative to SRCROOT.
    svn path=/trunk/vorbis/; revision=2014

--- win32/build_vorbis_dynamic_debug.bat
+++ win32/build_vorbis_dynamic_debug.bat
@@ -9,7 +9,7 @@ set OLDLIB=%LIB%
 
 call "c:\program files\microsoft visual studio\vc98\bin\vcvars32.bat"
 echo Setting include/lib paths for Vorbis
-set INCLUDE=%INCLUDE%;%SRCROOT%\ogg\include;c:\src\vorbis\include
+set INCLUDE=%INCLUDE%;%SRCROOT%\ogg\include;%SRCROOT%\vorbis\include
 set LIB=%LIB%;%SRCROOT%\ogg\win32\Dynamic_Debug
 echo Compiling...
 msdev vorbis_dynamic.dsp /useenv /make "vorbis_dynamic - Win32 Debug" /rebuild
--- win32/build_vorbisfile_static_debug.bat
+++ win32/build_vorbisfile_static_debug.bat
@@ -9,7 +9,7 @@ set OLDLIB=%LIB%
 
 call "c:\program files\microsoft visual studio\vc98\bin\vcvars32.bat"
 echo Setting include/lib paths for Vorbis
-set INCLUDE=%INCLUDE%;%SRCROOT%\ogg\include;c:\src\vorbis\include
+set INCLUDE=%INCLUDE%;%SRCROOT%\ogg\include;%SRCROOT%\vorbis\include
 set LIB=%LIB%;%SRCROOT%\ogg\win32\Static_Debug;%SRCROOT%\vorbis\win32\Dynamic_Debug
 echo Compiling...
 msdev vorbisfile_static.dsp /useenv /make "vorbisfile_static - Win32 Debug" /rebuild
