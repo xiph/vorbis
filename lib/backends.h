9e95db41ebc273bd436c9e5a7cfe7cc03a856f36
Monty <xiphmont@xiph.org>
2009-07-24 00:06:53 +0000

    Eliminate possibility of booklist overflow in res0/1/2 unpacking.
    svn path=/trunk/vorbis/; revision=16326

--- lib/backends.h
+++ lib/backends.h
@@ -111,7 +111,7 @@ typedef struct vorbis_info_residue0{
   int    partitions;       /* possible codebooks for a partition */
   int    groupbook;        /* huffbook for partitioning */
   int    secondstages[64]; /* expanded out to pointers in lookup */
-  int    booklist[256];    /* list of second stage books */
+  int    booklist[512];    /* list of second stage books */
 
   const float classmetric1[64];
   const float classmetric2[64];
