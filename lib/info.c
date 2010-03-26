a18132e733f9b0e37dc21e0c9eab4cf9bec4e6ed
Monty <xiphmont@xiph.org>
2010-03-26 01:54:25 +0000

    Leak fix in decode broke codebook load; fixed
    svn path=/trunk/vorbis/; revision=17051

--- lib/info.c
+++ lib/info.c
@@ -278,7 +278,7 @@ static int _vorbis_unpack_books(vorbis_info *vi,oggpack_buffer *opb){
   ci->books=oggpack_read(opb,8)+1;
   if(ci->books<=0)goto err_out;
   for(i=0;i<ci->books;i++){
-    ci->book_param[i]=_ogg_calloc(1,sizeof(*ci->book_param[i]));
+    ci->book_param[i]=vorbis_staticbook_unpack(opb);
     if(!ci->book_param[i])goto err_out;
   }
 
