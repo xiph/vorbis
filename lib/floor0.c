fcc8bcbe63a4328c6223af5d1a6890722b4b58b9
Tim Terriberry <tterribe@xiph.org>
2010-10-21 18:33:59 +0000

    Port r17545 from Tremor.
    Check that floor0 books all have a value mapping.
    
    svn path=/trunk/vorbis/; revision=17557

--- lib/floor0.c
+++ lib/floor0.c
@@ -91,6 +91,7 @@ static vorbis_info_floor *floor0_unpack (vorbis_info *vi,oggpack_buffer *opb){
   for(j=0;j<info->numbooks;j++){
     info->books[j]=oggpack_read(opb,8);
     if(info->books[j]<0 || info->books[j]>=ci->books)goto err_out;
+    if(ci->book_param[info->books[j]]->maptype==0)goto err_out;
   }
   return(info);
 
