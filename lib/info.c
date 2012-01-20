170e3ee8cdef06b12566eb0486de6acc6b995e79
Monty <xiphmont@xiph.org>
2012-01-20 07:35:26 +0000

    Commit fix to Moz Bugzilla #719612 (along with spec update in previous commit)
    svn path=/trunk/vorbis/; revision=18151

--- lib/info.c
+++ lib/info.c
@@ -31,8 +31,8 @@
 #include "misc.h"
 #include "os.h"
 
-#define GENERAL_VENDOR_STRING "Xiph.Org libVorbis 1.3.2"
-#define ENCODE_VENDOR_STRING "Xiph.Org libVorbis I 20101101 (Schaufenugget)"
+#define GENERAL_VENDOR_STRING "Xiph.Org libVorbis 1.3.3pre"
+#define ENCODE_VENDOR_STRING "Xiph.Org libVorbis I 20120120 (Omnipresent)"
 
 /* helpers */
 static int ilog2(unsigned int v){
