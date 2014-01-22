e6fe0d7455f37800b8d2fc3333084ac7df5b0424
Monty <xiphmont@xiph.org>
2014-01-22 18:03:15 +0000

    Bump vendor string for dot release.
    svn path=/trunk/vorbis/; revision=19058

--- lib/info.c
+++ lib/info.c
@@ -31,8 +31,8 @@
 #include "misc.h"
 #include "os.h"
 
-#define GENERAL_VENDOR_STRING "Xiph.Org libVorbis 1.3.3"
-#define ENCODE_VENDOR_STRING "Xiph.Org libVorbis I 20120203 (Omnipresent)"
+#define GENERAL_VENDOR_STRING "Xiph.Org libVorbis 1.3.4"
+#define ENCODE_VENDOR_STRING "Xiph.Org libVorbis I 20140122 (Turpakäräjiin)"
 
 /* helpers */
 static int ilog2(unsigned int v){
